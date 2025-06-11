9728d8de8b888ac79cadd1a81cad54b85c23fd7d
Indrajit Mali <220130107053@gecg28.ac.in>
2025-06-11 09:26:22 +0530

    Update Render deployment to use portable Java

--- start.js
+++ start.js
@@ -1,7 +1,8 @@
 const { exec } = require('child_process');
 
-// Run the Spring Boot JAR
-const jarProcess = exec('java -jar /out/artifacts/research_assistant_jar/research-assistant.jar' );
+// Use the Java binary from the downloaded JDK
+const javaBin = process.env.JAVA_HOME ? `${process.env.JAVA_HOME}/bin/java` : 'java';
+const jarProcess = exec(`${javaBin} -jar out/artifacts/research_assistant_jar/research-assistant.jar`);
 
 jarProcess.stdout.on('data', (data) => {
     console.log(data);
@@ -15,7 +16,6 @@ jarProcess.on('close', (code) => {
     console.log(`JAR process exited with code ${code}`);
 });
 
-// Keep the Node.js process alive
 process.on('SIGTERM', () => {
     jarProcess.kill();
     process.exit(0);
